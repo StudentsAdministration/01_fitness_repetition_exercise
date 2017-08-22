c23852ee3ac7b3c5f532495b51b887a28f2a4f5c
clbokea <clbo@kea.dk>
2017-08-22 12:33:33 +0200

    ignore in project

---
+++ code/FitnessExercises/src/dk/clbo/Employee.java
@@ -0,0 +1,24 @@
+package dk.clbo;
+
+public class Employee extends Person {
+
+
+    private int hours;
+    private int salary;
+
+    public Employee(String name, String cpr, int hours, int salary) {
+        super(name, cpr); // super klassens Konstruktør kaldes
+        this.hours = hours;
+        this.salary = salary;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+
+}
